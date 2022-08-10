5f01fe3de4f011743a72156c10044f463eed455e
Mezan <meaznurr@gmail.com>
2022-08-10 16:53:27 +0600

    selactor part

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('conected');
+
+// selactor
+
+const p1ScoreElm = document.querySelector('#p1Score');
+const p2ScoreElm = document.querySelector('#p2Score');
+const playingToElm = document.querySelector('.playingTo');
+const inputScoreElm = document.querySelector('#inputScore');
+const p1BtnElm = document.querySelector('#p1Btn');
+const p2BtnElm = document.querySelector('#p2Btn');
+const resetBtnElm = document.querySelector('#resetBtn');
+
+
+// Input and updating Playing to score
